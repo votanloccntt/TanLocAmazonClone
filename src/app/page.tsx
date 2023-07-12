b6ace827ee3cdb935ba6e77d6c888e133faa4214
votanloccntt <votanloccntt@gmail.com>
2023-07-12 11:10:44 +0700

    product list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
+import ProductCard from "@/components/ProductCard";
+import { prisma } from "@/lib/db/prisma";
+import Image from "next/image";
+import Link from "next/link";
 const HomePage = async () => {
-  return <div>HomePage</div>;
+  const products = await prisma.product.findMany({
+    orderBy: { id: "desc" },
+  });
+  return (
+    <div>
+      <div className="hero rounded-xl bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <Image
+            src={products[0].imageUrl}
+            alt={products[0].name}
+            width={400}
+            height={800}
+            className="w-full max-w-sm rounded-lg shadow-2xl"
+            priority
+          />
+          <div className="text-5xl font-bold">{products[0].name}</div>
+          <p className="py-6">{products[0].description}</p>
+          <Link
+            href={"/products/" + products[0].id}
+            className="btn btn-primary "
+          >
+            Check it out
+          </Link>
+        </div>
+      </div>
+      <div className="my-4 gird grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        {products.slice(1).map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
