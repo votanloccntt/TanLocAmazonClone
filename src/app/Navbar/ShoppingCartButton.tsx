9f6963f25fb613ac002d2ec22ba6063bccf4adc9
votanloccntt <votanloccntt@gmail.com>
2023-07-13 18:01:13 +0700

    add Cart page

--- src/app/Navbar/ShoppingCartButton.tsx
+++ src/app/Navbar/ShoppingCartButton.tsx
@@ -32,7 +32,12 @@ function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
               d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
             />
           </svg>
-          <span className="badge badge-sm indicator-item">
+
+          <span
+            className={`badge badge-sm indicator-item ${
+              cart?.size! > 0 ? "bg-red-400 text-white" : ""
+            }`}
+          >
             {cart?.size || 0}
           </span>
         </div>
@@ -42,9 +47,9 @@ function ShoppingCartButton({ cart }: ShoppingCartButtonProps) {
         className="card dropdown-content card-compact mt-3 w-52 bg-base-100 shadow z-30"
       >
         <div className="card-body">
-          <span className="text-lg font-bold">{cart?.size || 0}Items</span>
+          <span className="text-lg font-bold">{cart?.size || 0} Items</span>
           <span className="text-info">
-            Subtotal:{formatPrice(cart?.subtotal || 0)}
+            Subtotal: {formatPrice(cart?.subtotal || 0)}
           </span>
           <div className="card-actions">
             <Link
